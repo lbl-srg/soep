b6830e675c277c60abb79cd0797115eef492f5ca
Michael Wetter <MWetter@lbl.gov>
2016-09-23 19:06:23 -0700

    Corrected mathjax access and figure caption

--- doc-design/fixHtml.py
+++ doc-design/fixHtml.py
@@ -8,8 +8,11 @@ for fil in glob.glob("../docs/*.html"):
     lines = []
     with open(fil, 'r') as infile:
         for line in infile:
+            # This fixes wrong formatting for the use cases
             line = re.sub(r'<span class="caption-text">Figure \d: &lt;paragraph&gt;', ': ', line)
             line = re.sub(r'&lt;/paragraph&gt;</span>', '', line)
+            # This fixes wrong formatting for figures
+            line = re.sub(r'<span class="caption-text">Figure \d:', ':', line)
             lines.append(line)
 
     with open(fil, 'w') as outfile:
