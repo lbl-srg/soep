74690379a8ed111faed39bcd237a6808261443e4
JayHuLBL <JianjunHu@lbl.gov>
2017-11-15 17:24:31 -0800

    updated runCase script [ci skip]

--- models/benchmarks/SimulatorTemplate_JModelica.py
+++ models/benchmarks/SimulatorTemplate_JModelica.py
@@ -1,12 +1,15 @@
-import pymodelica 
+import pymodelica
 from pymodelica import compile_fmu
 from pyfmi import load_fmu
 
-fmu_name = compile_fmu("{{model_name}}", "{{model_name}}.mo", 
-                       version="{{fmi_version}}", 
-                       compiler_options={'extra_lib_dirs':["{{sim_lib_path}}","{{base_class}}"]},
+
+# define heap space
+pymodelica.environ['JVM_ARGS'] = '{{heap_space}}'
+
+fmu_name = compile_fmu("{{model}}", "{{model_name}}.mo",
+                       version={{fmi_version}},
+                       compiler_options={'extra_lib_dirs':["{{sim_lib_path}}"]},
                        compiler_log_level = "{{log_file}}")
 test_model = load_fmu(fmu_name)
-endTime = "{{end_time}}"
+endTime = {{end_time}}
 res = test_model.simulate(final_time=endTime)
-
--- models/benchmarks/caseSettings.py
+++ models/benchmarks/caseSettings.py
@@ -1,80 +1,82 @@
-END_TIME=2*24*3600
-
 def get_settings():
     ''' Return the simulation models that are used for benchmark study.
         The cases are stored in this function as they are used for simulation
         case settings and for the post processing.
     '''
-    SOLVER="radau"
-    BRANCH="master"
-    COMMIT="HEAD"
-    FROM_GIT_HUB = False
-    LOCAL_BUILDINGS_LIBRARY = "/home/jianjun/GitFolder/modelica-buildings/Buildings"
-    JMODELICA_INST = "/home/jianjun/proj/JModelica"
+    runSettings = {
+        "SOLVER": "radau",
+        "END_TIME": 2*24*3600.0,
+        "BRANCH": "master",
+        "COMMIT": "HEAD",
+        "FROM_GIT_HUB": False,
+        "LOCAL_BUILDINGS_LIBRARY": "/home/jianjun/GitFolder/modelica-buildings/Buildings",
+        "JMODELICA_INST": "/home/jianjun/proj/JModelica2.0",
+        "Heap_Space": "7200m"
+        }
 
     settings = list()
     settings.append(\
         {'model': "Buildings.Examples.HydronicHeating.TwoRoomsWithStorage",
-         "solver": SOLVER,
+         "solver": runSettings['SOLVER'],
          "start_time": 0,
-         "stop_time": END_TIME})
+         "stop_time": runSettings['END_TIME']})
     settings.append(\
         {'model': "Buildings.Examples.DualFanDualDuct.ClosedLoop",
-         "solver": SOLVER,
+         "solver": runSettings['SOLVER'],
          "start_time": 0,
-         "stop_time": END_TIME})
+         "stop_time": runSettings['END_TIME']})
     settings.append(\
         {'model': "Buildings.Examples.VAVReheat.ClosedLoop",
-         "solver": SOLVER,
+         "solver": runSettings['SOLVER'],
          "start_time": 0,
-         "stop_time": END_TIME})
+         "stop_time": runSettings['END_TIME']})
     settings.append(\
         {'model': "Buildings.Examples.ChillerPlant.DataCenterDiscreteTimeControl",
-         "solver": SOLVER,
+         "solver": runSettings['SOLVER'],
          "start_time": 0,
-         "stop_time": END_TIME})
+         "stop_time": runSettings['END_TIME']})
     settings.append(\
         {'model': "Buildings.Examples.ChillerPlant.DataCenterContinuousTimeControl",
-         "solver": SOLVER,
+         "solver": runSettings['SOLVER'],
          "start_time": 0,
-         "stop_time": END_TIME})
+         "stop_time": runSettings['END_TIME']})
     settings.append(\
         {'model': "Buildings.Examples.VAVReheat.ASHRAE2006",
-         "solver": SOLVER,
+         "solver": runSettings['SOLVER'],
          "start_time": 0,
-         "stop_time": END_TIME})
+         "stop_time": runSettings['END_TIME']})
     settings.append(\
         {'model': "Buildings.Examples.VAVReheat.Guideline36",
-         "solver": SOLVER,
+         "solver": runSettings['SOLVER'],
          "start_time": 0,
-         "stop_time": END_TIME})
+         "stop_time": runSettings['END_TIME']})
     settings.append(\
         {'model': "Buildings.Experimental.DistrictHeatingCooling.Examples.HeatingCoolingHotWater3Clusters",
-         "solver": SOLVER,
+         "solver": runSettings['SOLVER'],
          "start_time": 0,
-         "stop_time": END_TIME})
+         "stop_time": runSettings['END_TIME']})
     settings.append(\
         {'model': "Buildings.Examples.ScalableBenchmarks.BuildingVAV.Examples.OneFloor_OneZone",
-         "solver": SOLVER,
+         "solver": runSettings['SOLVER'],
          "start_time": 0,
-         "stop_time": END_TIME})
+         "stop_time": runSettings['END_TIME']})
     settings.append(\
         {'model': "Buildings.Examples.ScalableBenchmarks.BuildingVAV.Examples.TwoFloor_TwoZone",
-         "solver": SOLVER,
+         "solver": runSettings['SOLVER'],
          "start_time": 0,
-         "stop_time": END_TIME})
+         "stop_time": runSettings['END_TIME']})
     settings.append(\
         {'model': "Buildings.Air.Systems.SingleZone.VAV.Examples.ChillerDXHeatingEconomizer",
-         "solver": SOLVER,
+         "solver": runSettings['SOLVER'],
          "start_time": 0,
-         "stop_time": END_TIME})
+         "stop_time": runSettings['END_TIME']})
 
 
     settings = list()
     settings.append(\
-        {'model': "Buildings.Air.Systems.SingleZone.VAV.Examples.ChillerDXHeatingEconomizer",
-         "solver": SOLVER,
+        {'model': "Buildings.Examples.ScalableBenchmarks.BuildingVAV.Examples.OneFloor_OneZone",
+         "solver": runSettings['SOLVER'],
          "start_time": 0,
-         "stop_time": END_TIME})
+         "stop_time": runSettings['END_TIME']})
 
-    return settings, BRANCH, COMMIT, FROM_GIT_HUB, LOCAL_BUILDINGS_LIBRARY, JMODELICA_INST
+    return settings, runSettings
